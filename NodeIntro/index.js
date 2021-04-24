991f690009f841c3c28cae5a8afb2acd295386b1
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-04-24 16:07:16 +0500

    Lecture 3
    Node.js Introduction.

---
+++ NodeIntro/index.js
@@ -0,0 +1,13 @@
+// Iteration 1
+// console.log("Hello Class!");
+
+// Iteration 2
+// const superheroes = require("superheroes");
+//
+// var mySuperheroname = superheroes.random();
+//
+// console.log(mySuperheroname);
+
+// Iteration 3
+const fs = require("fs");
+fs.copyFileSync('test1.txt', 'test2.txt');
