e37b1d1ae05ddf0b9fb53cd0fd3f8bdffbd33994
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-04-24 14:47:39 +0500

    Lecture 2
    Introduction to JS

---
+++ JS Intro/index.js
@@ -0,0 +1,98 @@
+// 5+7
+// 12
+// 5-4
+// 1
+// 4*3
+// 12
+// 4/2
+// 2
+// 4%3
+// 1
+
+// var a = "Jahanzeb";
+
+// typeof(a);
+// "string"
+
+// var b = 5;
+// undefined
+// typeof(b);
+// "number"
+// var c = true;
+
+// typeof(c);
+// "boolean"
+
+// "My name is " + a;
+// "My name is Jahanzeb"
+
+// a.length;
+// 8
+
+// a.slice(3, 5);
+// "an"
+// alert("My name is " + a);
+
+// alert("My name is " + a);
+
+// alert("My name is " + a);
+// alert("Welcome to today's class");
+
+// alert("My name is \"" + a + "\"");
+// alert("Welcome to today's class");
+
+/* This is a multiline comment */
+
+// var x = 1;
+// x = x + 1;
+// x++;
+// ++x;
+// alert(x++);
+// alert(++x);
+
+// var randomVal = Math.floor(Math.random() * 100) + 1;
+
+// if (randomVal >= 80) {
+//     alert("You have scored A " + "as your score is " + randomVal);
+// } else if (randomVal > 50 && randomVal < 80) {
+//     alert("You have passed " + "as your score is " + randomVal);
+// } else {
+//     alert("You will fail " + "as you have scored " + randomVal);
+// }
+
+// var cars = ["Toyota", "Honda", "Kia"];
+// cars.length;
+// cars.push("Dahitsu");
+// // alert(cars[0]);
+
+// cars.sort();
+// cars.reverse();
+
+// for(var i=0; i<cars.length; i++) {
+//     alert(cars[i]);
+// }
+
+function Add(n1, n2) {
+    return(n1+n2);
+}
+
+function Add() {
+    alert(2-1);
+}
+
+var result = Add(3, 4);
+alert(result);
+Add();
+
+// class Car {
+//   constructor(name, year) {
+//     this.name = name;
+//     this.year = year;
+//   }
+// }
+
+// let myCar1 = new Car("Honda Civic", 2009);
+// let myCar2 = new Car("Honda City", 2013);
+// let myCar3 = new Car("Toyota Grande", 2018);
+
+// alert(myCar1.name + " " + myCar1.year);
